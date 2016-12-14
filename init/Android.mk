1537557d519eaf444fe0680a2d2a5b6d12f9cfce
ZIM <zim555@mail.ru>
2016-12-14 17:52:35 +0200

    Fix for cm14.1

--- init/Android.mk
+++ init/Android.mk
@@ -5,7 +5,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_C_INCLUDES := system/core/init
 LOCAL_CFLAGS := -Wall -DANDROID_TARGET=\"$(TARGET_BOARD_PLATFORM)\"
-LOCAL_SRC_FILES := init_fortuna3gfz.cpp
-LOCAL_MODULE := libinit_fortuna3gfz
+LOCAL_SRC_FILES := init_fortunave3g.cpp
+LOCAL_MODULE := libinit_fortunave3g
 
 include $(BUILD_STATIC_LIBRARY)
