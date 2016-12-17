75f73332342053d2baff84278774d173bc967e0a
ZIM <zim555@mail.ru>
2016-12-17 14:47:12 +0200

    Update nfc for building cm14.1

--- device.mk
+++ device.mk
@@ -8,22 +8,21 @@ DEVICE_PACKAGE_OVERLAYS +=  device/samsung/fortuna3gfz/overlay
 
 # Permissions
 PRODUCT_COPY_FILES += \
-	frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml
+    frameworks/base/nfc-extras/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
+    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
+    frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
 
 #NFC
-PRODUCT_PACKAGES += \
-    Tag \
-    com.android.nfc_extras \
-    com.android.nfc.helper \
+libnfc_nci \
     NfcNci \
-    libnfc-nci
+    Tag \
+    com.android.nfc_extras
 
 # NFC prebuilt files
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/nfc/libnfc-sec-hal.conf:system/etc/libnfc-sec-hal.conf \
     $(LOCAL_PATH)/nfc/libnfc-sec.conf:system/etc/libnfc-brcm.conf \
-    $(LOCAL_PATH)/nfc/nfcee_access.xml:system/etc/nfcee_access.xml \
-    packages/apps/Nfc/migrate_nfc.txt:system/etc/updatecmds/migrate_nfc.txt
+    $(LOCAL_PATH)/nfc/nfcee_access.xml:system/etc/nfcee_access.xml
 
 # NFC workaround
 PRODUCT_COPY_FILES += \
